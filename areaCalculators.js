da25917da3eeda0c460fd86008c5a791cd33d87e
TakNg <tak_ng@yahoo.com>
2017-06-08 09:35:05 +0000

    Adds functions exercises

--- areaCalculators.js
+++ areaCalculators.js
@@ -1,12 +1,21 @@
 function calculateRectangleArea(length, width) {
+  if (length < 0 || width < 0) {
+    return undefined;
+  }
   return length * width;
 }
 
 function calculateTriangleArea(base, height) {
+  if (base < 0 || height < 0) {
+    return undefined;
+  }
   return base * height / 2;
 }
 
 function calculateCircleArea(radius) {
+  if (radius < 0) {
+    return undefined;
+  }
   return Math.pow(radius, 2) * Math.PI;
 }
 
