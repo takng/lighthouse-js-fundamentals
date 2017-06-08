b68ec677a9189ba610316d93a59e454098a8ed94
TakNg <tak_ng@yahoo.com>
2017-06-08 07:57:33 +0000

    Adds functions exercises

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,6 +1,6 @@
 function ageCalculator (name, year1, year2)
 {
-	return name + " is " + (year2-year1) + " years old."
+  return name + " is " + (year2 - year1) + " years old."
 }
 
 console.log(ageCalculator("Miranda", 1983, 2015));
