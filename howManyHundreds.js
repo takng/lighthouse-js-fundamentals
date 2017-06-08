f0d40cbc61874c16af5e3d980045c1b3f67e7ba4
TakNg <tak_ng@yahoo.com>
2017-06-08 07:48:46 +0000

    Adds functions exercises

---
+++ howManyHundreds.js
@@ -0,0 +1,11 @@
+function howManyHundreds (num)
+{
+	return parseInt(num/100);
+}
+
+console.log(howManyHundreds(1010));
+console.log(howManyHundreds(894));
+console.log(howManyHundreds(520));
+console.log(howManyHundreds(99));
+console.log(howManyHundreds(0));
+
