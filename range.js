22d660dad02bf2f738152a33c0032942f98b5a0b
TakNg <tak_ng@yahoo.com>
2017-06-09 08:27:50 +0000

    Adds functions exercises

---
+++ range.js
@@ -0,0 +1,18 @@
+function range(start, end, step) {
+  var some_array = [];
+
+  if (start === undefined || end === undefined || step === undefined || start > end || step < 0) {
+    return some_array;
+  } 
+
+  for (var i = start; i <= end; i += step) {
+    some_array.push(i);
+  }
+
+  return some_array;
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+
